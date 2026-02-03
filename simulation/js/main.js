12722a415f6fcf5fedebb3aec1fadbb0f2579d0b
vleadadmin <admin@vlabs.ac.in>
2026-02-03 07:47:00 +0000

    https://virtual-labs.github.io/virtual-labs/exp-ordinary-linear-differential-dei click on the link to test your code.

---
+++ simulation/js/main.js
@@ -0,0 +1,254 @@
+
+
+function ques1(){
+      const selected = document.getElementById("q1").value;
+  const result = document.getElementById("result1");
+  if(selected === "sel"){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "c") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct!";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer: Any function";
+    result.style.color = "red";
+    
+  }}
+}
+
+function ques2(){
+      const selected = document.getElementById("q2").value;
+  const result = document.getElementById("result2");
+  if(selected === "sel"){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "a") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct!.";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer: All the solutions";
+    result.style.color = "red";
+    
+  }}
+}
+
+function ques3(){
+      const selected = document.getElementById("q3").value;
+  const result = document.getElementById("result3");
+  if(selected === "sel"){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "a") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct! ";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer: A specific solution";
+    result.style.color = "red";
+    
+  }}
+}
+
+function ques4(){
+      const selected = document.getElementById("q4").value;
+  const result = document.getElementById("result4");
+  if(selected === "sel"){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "c") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct! ";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer: CF + PI";
+    result.style.color = "red";
+    
+  }}
+}
+
+
+function ques4_1(){
+      const selected = document.getElementById("q4_1").value;
+  const result = document.getElementById("result4_1");
+  if(selected === "sel"){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "a") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct! ";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer: all solutions";
+    result.style.color = "red";
+    
+  }}
+}
+
+function ques4_2(){
+      const selected = document.getElementById("q4_2").value;
+  const result = document.getElementById("result4_2");
+  if(selected === ""){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "c") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct! ";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer: All solutions are";
+    result.style.color = "red";
+    
+  }}
+}
+
+function ques5(){
+      const selected = document.getElementById("q5").value;
+  const result = document.getElementById("result5");
+  if(selected === ""){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "a") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct!";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer: CF";
+    result.style.color = "red";
+    
+  }}
+}
+
+function ques5_1(){
+      const selected = document.getElementById("q5_1").value;
+  const result = document.getElementById("result5_1");
+  if(selected === ""){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "a") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct!";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer: CF";
+    result.style.color = "red";
+    
+  }}
+}
+
+
+
+// -------------UNDERSTANDING SECTION-----------------------
+
+function unst_1(){
+      const selected = document.getElementById("unst_1").value;
+  const result = document.getElementById("resunst_1");
+  if(selected === ""){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "d") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct!";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer: given by (i) ";
+    result.style.color = "red";
+    
+  }}
+}
+
+function unst_2(){
+      const selected = document.getElementById("unst_2").value;
+  const result = document.getElementById("resunst_2");
+  if(selected === ""){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "a") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct!";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer: T(y₀) = f(x) ";
+    result.style.color = "red";
+    
+  }}
+}
+
+function unst_3(){
+      const selected = document.getElementById("unst_3").value;
+  const result = document.getElementById("resunst_3");
+  if(selected === ""){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "b") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct!";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer: ker T ";
+    result.style.color = "red";
+    
+  }}
+}
+
+function unst_4(){
+      const selected = document.getElementById("unst_4").value;
+  const result = document.getElementById("resunst_4");
+  if(selected === ""){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "b") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct!";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer: y₀ ";
+    result.style.color = "red";
+    
+  }}
+}
+
+function unst_5(){
+      const selected = document.getElementById("unst_5").value;
+  const result = document.getElementById("resunst_5");
+  if(selected === ""){
+    result.textContent = "Please select the option!";
+    result.style.color = "blue";
+  }
+else{
+  if (selected === "a") {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>✅ Correct!";
+    result.style.color = "green";
+   
+  } else {
+    result.innerHTML = "<span style='color:black;'>Answer:</span>❌ Incorrect! Correct Answer:   Complete Integral ";
+    result.style.color = "red";
+    
+  }}
+}
+
